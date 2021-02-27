29dc93b0a37398040d901033631ac12484de6090
Sergey Popov <tlc2engineer@gmail.com>
2021-02-27 16:11:02 +0300

    377607

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"Golden/api"
 	"Golden/models"
+	"sync"
 
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 )
+
+var numLic int = 0
+var mu = new(sync.Mutex)
 
 func main() {
 
@@ -32,16 +36,35 @@ func main() {
 	api.GetBasicPath()
 	chTrlist := make(chan models.TreasureList, 5)
 	chCoin := make(chan uint32, 100)
-	go PostCashG(chTrlist, chCoin)
+	go PostCashG(chTrlist, chCoin, true)
+	go PostCashG(chTrlist, chCoin, false)
 	chDig := make(chan DigData, 5)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
-	go DigG(chDig, chTrlist)
+	chLic := make(chan *models.License, 10)
+	chUsedLic := make(chan *int64)
+
+	go func() {
+		for l := range chUsedLic {
+			if *l > 0 {
+				mu.Lock()
+				numLic--
+				mu.Unlock()
+			}
+
+		}
+	}()
+	go LicGor(chCoin, chLic)
+	go LicGor(chCoin, chLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+	go DigG(chDig, chTrlist, chLic, chUsedLic)
+
 	//------------тестируем explore-------------
 	for x := 1; x < 3500; x++ {
 		for y := 1; y < 3500; y++ {
@@ -74,18 +97,22 @@ func updateLicense() *models.License {
 }
 
 /*PostCashG - горутина отправки сообщений*/
-func PostCashG(ch chan models.TreasureList, chCoins chan uint32) {
+func PostCashG(ch chan models.TreasureList, chCoins chan uint32, toLic bool) {
 	for tlist := range ch {
 		for _, treasure := range tlist {
-			_, err := api.PostCash(treasure)
+			w, err := api.PostCash(treasure)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Post cash err", err)
+			} else {
+				if w != nil && toLic {
+					for _, coin := range *w {
+						if len(chCoins) < 90 {
+							chCoins <- coin
+						}
+					}
+				}
 			}
-			// for _, coin := range *w {
-			// 	if len(chCoins) < 90 {
-			// 		chCoins <- coin
-			// 	}
-			// }
+
 		}
 	}
 }
@@ -96,20 +123,24 @@ type DigData struct {
 }
 
 /*DigG - горутина копания*/
-func DigG(ch chan DigData, cht chan models.TreasureList) {
+func DigG(ch chan DigData, cht chan models.TreasureList, chLic chan *models.License, chUsedLic chan *int64) {
 	var license *models.License // лицензия
 	for ddata := range ch {
 		trCount := ddata.amount // число ненайденных сокровиц
 		depth := 1              //глубина
 		for trCount > 0 && depth <= 10 {
 			if license == nil || *license.DigAllowed <= *license.DigUsed {
-				license = updateLicense()
+				if license != nil {
+					chUsedLic <- license.ID // использованная лицензия
+				}
+				license = <-chLic // полцчаем лицензию
 			}
 			tlist, err := api.DigPost(int64(depth), *license.ID, ddata.x, ddata.y)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Dig err", err)
 			} else {
 				*license.DigUsed++
+
 				depth++
 				if tlist != nil {
 					trCount--
@@ -118,5 +149,37 @@ func DigG(ch chan DigData, cht chan models.TreasureList) {
 			}
 		}
 
+	}
+}
+
+/*LicGor - лицензии*/
+func LicGor(chCoin chan uint32, chLic chan *models.License) {
+	var coin uint32
+	var wallet models.Wallet
+
+	var numCoin, numFree int
+	for {
+		if numLic >= 10 {
+			time.Sleep(time.Millisecond)
+			continue
+		}
+		select {
+
+		case coin = <-chCoin:
+			wallet = models.Wallet{} // платная
+			wallet = append(wallet, coin)
+			numCoin++
+		default:
+			wallet = models.Wallet{} // бесплатная
+			numFree++
+		}
+		lic, err := api.PostLicense(wallet)
+		if err != nil {
+		} else {
+			mu.Lock()
+			numLic++
+			mu.Unlock()
+			chLic <- lic
+		}
 	}
 }
